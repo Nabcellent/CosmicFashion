15a31110b7e00ab6728b803d6ec7cf05744e193c
Nabcellent <nabcellent.dev@gmail.com>
2021-10-22 05:57:50 +0300

    Added Previously ignored files

---
+++ app/Config/Eloquent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Eloquent extends BaseConfig {
+    public string $defaultView = '\Nabz\Views\default';
+
+    public string $simpleDefaultView = '\Nabz\Views\simpleDefault';
+}
