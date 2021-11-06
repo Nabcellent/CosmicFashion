c66f639c69fc20e538def66dafc5e65d16b94655
Nabcellent <nabcellent.dev@gmail.com>
2021-11-06 14:17:10 +0300

    Added shortcut Icon to layouts

--- app/Views/layouts/guest.php
+++ app/Views/layouts/guest.php
@@ -6,6 +6,12 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?= env('app.name', 'CosmicFashion.') . ($title ?? '') ?></title>
 
+	<link rel="apple-touch-icon" sizes="180x180" href="/images/dash/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/dash/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/dash/favicons/favicon-16x16.png">
+	<link rel="shortcut icon" type="image/x-icon" href="/images/dash/favicons/favicon.ico">
+	<link rel="manifest" href="/images/dash/favicons/manifest.json">
+
     <?= $this->include('partials/links') ?>
 
 	<link rel="stylesheet" href="/css/auth.css">
--- app/Views/layouts/master.php
+++ app/Views/layouts/master.php
@@ -6,7 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?= env('app.name', 'CosmicFashion.') . ($title ?? '') ?></title>
 
-	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<link rel="apple-touch-icon" sizes="180x180" href="/images/dash/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/dash/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/dash/favicons/favicon-16x16.png">
+	<link rel="shortcut icon" type="image/x-icon" href="/images/dash/favicons/favicon.ico">
+	<link rel="manifest" href="/images/dash/favicons/manifest.json">
 
 	<link rel="stylesheet" href="/css/second.css">
 	<link rel="stylesheet" href="/css/shop.css">
