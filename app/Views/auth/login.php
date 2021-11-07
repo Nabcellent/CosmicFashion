83ded35d265d3780922a0156634fd8250f42797b
Nabcellent <nabcellent.dev@gmail.com>
2021-11-07 22:06:52 +0300

    Modified Auth Designs

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -10,47 +10,69 @@
 		<div class="container">
 			<div class="d-flex justify-content-center row">
 				<div class="col-auto col-xl-6">
-					<h4 class="fw-bold" style="margin-bottom:100px"><a href="<?= route_to('home') ?>"><?= env('app.name', 'CosmicFashion.') ?></a></h4>
-					<h5 class="fw-bold mb-5">Login</h5>
-
-                    <?php if(session()->getFlashdata('msg')): ?>
-						<div class="alert alert-danger alert-dismissible fade show" role="alert">
-							<strong>Holy guacamole!</strong><hr/>
-                            <?= session()->getFlashdata('msg') ?>
-							<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+					<div class="card rounded-7">
+						<div class="card-header bg-circle-shape bg-shape text-center p-3 rounded-7">
+							<a class="fw-bolder fs-4 z-index-1 position-relative link-light light"
+							   href="<?= route_to('home') ?>"><?= env('app.name', 'CosmicFashion.') ?></a>
 						</div>
-                    <?php endif; ?>
+						<div class="card-body p-3 p-md-4">
+							<div class="d-flex justify-content-between align-items-center mb-5">
+								<h5 class="fw-bold">Sign In</h5>
+								<small>New user? <a href="<?= route_to('register') ?>">Create account</a></small>
+							</div>
 
-                    <?= view('App\auth\_message_block') ?>
+                            <?php if(session()->getFlashdata('msg')): ?>
+								<div class="alert alert-danger alert-dismissible fade show" role="alert">
+									<strong>Holy guacamole!</strong>
+									<hr/>
+                                    <?= session()->getFlashdata('msg') ?>
+									<button type="button" class="btn-close" data-bs-dismiss="alert"
+									        aria-label="Close"></button>
+								</div>
+                            <?php endif; ?>
 
-					<form id="login" action="" method="POST">
-                        <?= csrf_field() ?>
+                            <?= view('App\auth\_message_block') ?>
 
-						<div class="form-group">
-							<small class="fw-bold">Email</small>
-							<input type="email" name="email" placeholder="Email" value="<?= old('email') ?>" required="" aria-label
-							       class="w-100 form-control"/>
-						</div>
-						<small class="fw-bold">Password</small>
-						<div class="input-group">
-							<input type="password" name="password" placeholder="Password" required aria-label
-							       class="form-control"/>
-							<span class="input-group-text border-right-0 rounded-0 show-password" title="Show Password" style="cursor: pointer">
+							<form id="login" action="" method="POST">
+                                <?= csrf_field() ?>
+
+								<div class="form-group">
+									<small class="fw-bold">Email</small>
+									<input type="email" name="email" placeholder="Email" value="<?= old('email') ?>"
+									       required="" aria-label
+									       class="w-100 form-control"/>
+								</div>
+								<small class="fw-bold">Password</small>
+								<div class="input-group">
+									<input type="password" name="password" placeholder="Password" required aria-label
+									       class="form-control"/>
+									<span class="input-group-text border-right-0 rounded-0 show-password"
+									      title="Show Password" style="cursor: pointer">
 									<i class="bi bi-eye"></i></span>
+								</div>
+								<div class="row justify-content-between small my-3">
+									<div class="col-auto">
+										<div class="form-check mb-0"><input class="form-check-input" type="checkbox"
+										                                    id="split-checkbox"><label
+													class="form-check-label mb-0" for="split-checkbox">Remember
+												me</label></div>
+									</div>
+									<div class="col-auto"><a class="fs--1" href="forgot-password.html">Forgot
+											Password?</a></div>
+								</div>
+								<div class="d-grid">
+									<button type="submit"
+									        class="Login_button__28f5q fw-bold text-uppercase btn btn-primary ld-ext-right">
+										Sign In <i class="fas fa-key"></i>
+										<span class="ld ld-ring ld-spin"></span>
+									</button>
+								</div>
+							</form>
+							<footer class="d-flex justify-content-center small auth_footer">
+								<a href="javascript:void(0)">Terms & Conditions</a>
+							</footer>
 						</div>
-						<div class="d-flex justify-content-between align-items-center mt-5">
-							<a href="<?= route_to('register') ?>">Create an account</a>
-							<button type="submit" class="Login_button__28f5q fw-bold text-uppercase btn btn-primary ld-ext-right">
-								Sign In <i class="fas fa-key"></i>
-								<span class="ld ld-ring ld-spin"></span>
-							</button>
-						</div>
-					</form>
-					<footer class="d-flex justify-content-between Login_footer__3RA6w">
-						<a href="javascript:void(0)">Terms & Conditions</a>
-						<a href="javascript:void(0)">Privacy Policy</a>
-						<a href="javascript:void(0)">Forgot password</a>
-					</footer>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -61,46 +83,50 @@
 <?= $this->section('scripts') ?>
 	<script src="/vendor/jquery/jqueryValidation.js"></script>
 	<script>
-		$(() => {
+        $(() => {
             $('#login').validate({
-	            rules: {
+                rules: {
                     email: {
                         required: true,
-	                    email: true
+                        email: true
                     },
-		            password: 'required'
-	            },
-	            submitHandler: form => {
+                    password: 'required'
+                },
+                submitHandler: form => {
                     const data = {};
                     $(form).serializeArray().map(input => data[input.name] = input.value)
 
-		            const submitButton = $(form).find($('button[type="submit"]'))
+                    const submitButton = $(form).find($('button[type="submit"]'))
 
-		            $.ajax({
-			            data: data,
-			            url: `<?= route_to('login') ?>`,
-			            method: 'POST',
+                    $.ajax({
+                        data: data,
+                        url: `<?= route_to('login') ?>`,
+                        method: 'POST',
                         beforeSend: () => submitButton.prop('disabled', true).addClass('running'),
-			            success: response => {
+                        success: response => {
                             const result = JSON.parse(response)
 
-				            if(result.status) {
+                            if (result.status) {
                                 location.href = result.url
-				            } else if(result.message) {
-                                toast({msg:result.message, type: 'warning', duration: 10, position: 'left'})
-				            } else {
-                                toast({msg:`Error: unable to log you in. Kindly contact admin.`, type: 'warning', position:'left'})
-				            }
-			            },
+                            } else if (result.message) {
+                                toast({msg: result.message, type: 'warning', duration: 10, position: 'left'})
+                            } else {
+                                toast({
+                                    msg: `Error: unable to log you in. Kindly contact admin.`,
+                                    type: 'warning',
+                                    position: 'left'
+                                })
+                            }
+                        },
                         complete: (xhr) => {
                             let err = eval("(" + xhr.responseText + ")");
 
-                            if(err.status !== true) submitButton.prop('disabled', false).removeClass('running')
+                            if (err.status !== true) submitButton.prop('disabled', false).removeClass('running')
                         }
-		            })
-	            }
+                    })
+                }
             })
-		})
+        })
 	</script>
 <?= $this->endSection() ?>
 <?= $this->endSection() ?>
--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -8,97 +8,107 @@
 <div class="d-flex flex-column justify-content-center align-items-center h-100 col-12 col-md-6">
 	<div class="container">
 		<div class="d-flex justify-content-center row">
-			<div class="col-auto col-lg-8">
-				<h4 class="fw-bold" style="margin-bottom:100px"><a
-							href="<?= route_to('home') ?>"><?= env('app.name', 'CosmicFashion.') ?></a></h4>
-				<h5 class="fw-bold mb-5">Sign Up</h5>
-
-                <?php if(session()->getFlashdata('msg')): ?>
-					<div class="alert alert-danger alert-dismissible fade show" role="alert">
-						<strong>Holy guacamole!</strong>
-						<hr/>
-						<ul>
-                            <?php foreach(session()->getFlashdata('msg') as $msg): ?>
-								<li><?= $msg ?></li>
-                            <?php endforeach; ?>
-						</ul>
-						<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			<div class="col-auto col-lg-8 rounded-7">
+				<div class="card">
+					<div class="card-header bg-circle-shape bg-shape text-center p-3 rounded-7">
+						<a class="fw-bolder fs-4 z-index-1 position-relative link-light light"
+						   href="<?= route_to('home') ?>"><?= env('app.name', 'CosmicFashion.') ?></a>
 					</div>
-                <?php endif; ?>
+					<div class="card-body p-3 p-md-4">
+						<div class="d-flex justify-content-between align-items-center mb-5">
+							<h5 class="fw-bold">Sign Up</h5>
+							<small>Already a user? <a href="<?= route_to('login') ?>">Sign In</a></small>
+						</div>
 
-                <?= view('App\auth\_message_block') ?>
-
-				<form id="register" action="" method="POST" class="w-100">
-                    <?= csrf_field() ?>
-
-					<div class="row mb-2">
-						<div class="col form-group">
-							<small class="fw-bold">First name</small>
-							<input type="text" name="first_name" placeholder="John" value="<?= old('first_name') ?>"
-							       required aria-label
-							       class="w-100 form-control"/>
-						</div>
-						<div class="col form-group">
-							<small class="fw-bold">Last name</small>
-							<input type="text" name="last_name" placeholder="Doe" value="<?= old('last_name') ?>"
-							       required aria-label
-							       class="w-100 form-control"/>
-						</div>
-					</div>
-					<div class="form-group">
-						<small class="fw-bold">Email</small>
-						<input type="email" name="email" placeholder="Email address" value="<?= old('email') ?>"
-						       required aria-label
-						       class="w-100 form-control"/>
-					</div>
-					<div class="row justify-content-center mb-2">
-						<div class="col-12 text-center"><small class="fw-bold">Gender</small></div>
-						<div class="col-auto form-check form-check-inline">
-							<input class="form-check-input" type="radio" name="gender" id="male" value="male">
-							<label class="form-check-label" for="male">Male</label>
-						</div>
-						<div class="col-auto form-check form-check-inline">
-							<input class="form-check-input" type="radio" name="gender" id="female" value="female">
-							<label class="form-check-label" for="female">Female</label>
-						</div>
-					</div>
-					<div class="row mb-2">
-						<div class="col">
-							<small class="fw-bold">Password</small>
-							<div class="input-group">
-								<input type="password" name="password" placeholder="Password" id="password" required
-								       aria-label
-								       class="form-control"/>
-								<span class="input-group-text border-right-0 rounded-0 show-password"
-								      title="Show Password" style="cursor: pointer">
-									<i class="bi bi-eye"></i></span>
+                        <?php if(session()->getFlashdata('msg')): ?>
+							<div class="alert alert-danger alert-dismissible fade show" role="alert">
+								<strong>Holy guacamole!</strong>
+								<hr/>
+								<ul>
+                                    <?php foreach(session()->getFlashdata('msg') as $msg): ?>
+										<li><?= $msg ?></li>
+                                    <?php endforeach; ?>
+								</ul>
+								<button type="button" class="btn-close" data-bs-dismiss="alert"
+								        aria-label="Close"></button>
 							</div>
-						</div>
-						<div class="col">
-							<small class="fw-bold">Confirm Password</small>
-							<div class="input-group">
-								<input type="password" name="password_confirmation" placeholder="Password" required
-								       aria-label
-								       class="form-control"/>
-								<span class="input-group-text border-right-0 rounded-0 show-password"
-								      title="Show Password" style="cursor: pointer">
-									<i class="bi bi-eye"></i></span>
-							</div>
-						</div>
-					</div>
-					<div class="d-flex justify-content-between align-items-center mt-5">
-						<a href="<?= route_to('login') ?>">Sign In to your account</a>
-						<button type="submit"
-						        class="fw-bold text-uppercase Register_button__1hHYm btn btn-primary ld-ext-right">
-							SIGN UP <i class="fas fa-user-plus"></i><span class="ld ld-ring ld-spin"></span>
-						</button>
-					</div>
-				</form>
-				<footer style="margin-top:100px" class="d-flex justify-content-between Register_footer__3THtR">
-					<a href="javascript:void(0)">Terms & Conditions</a>
-					<a href="javascript:void(0)">Privacy Policy</a><a href="register.html#">Help</a>
-				</footer>
+                        <?php endif; ?>
 
+						<form id="register" action="" method="POST" class="w-100">
+                            <?= csrf_field() ?>
+
+							<div class="row mb-2">
+								<div class="col form-group">
+									<small class="fw-bold">First name</small>
+									<input type="text" name="first_name" placeholder="John"
+									       value="<?= old('first_name') ?>"
+									       required aria-label
+									       class="w-100 form-control"/>
+								</div>
+								<div class="col form-group">
+									<small class="fw-bold">Last name</small>
+									<input type="text" name="last_name" placeholder="Doe"
+									       value="<?= old('last_name') ?>"
+									       required aria-label
+									       class="w-100 form-control"/>
+								</div>
+							</div>
+							<div class="form-group">
+								<small class="fw-bold">Email</small>
+								<input type="email" name="email" placeholder="Email address" value="<?= old('email') ?>"
+								       required aria-label
+								       class="w-100 form-control"/>
+							</div>
+							<div class="row justify-content-center mb-2">
+								<div class="col-12 text-center"><small class="fw-bold">Gender</small></div>
+								<div class="col-auto form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="gender" id="male" value="male">
+									<label class="form-check-label" for="male">Male</label>
+								</div>
+								<div class="col-auto form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="gender" id="female"
+									       value="female">
+									<label class="form-check-label" for="female">Female</label>
+								</div>
+							</div>
+							<div class="row mb-4">
+								<div class="col">
+									<small class="fw-bold">Password</small>
+									<div class="input-group">
+										<input type="password" name="password" placeholder="Password" id="password"
+										       required
+										       aria-label
+										       class="form-control"/>
+										<span class="input-group-text border-right-0 rounded-0 show-password"
+										      title="Show Password" style="cursor: pointer">
+									<i class="bi bi-eye"></i></span>
+									</div>
+								</div>
+								<div class="col">
+									<small class="fw-bold">Confirm Password</small>
+									<div class="input-group">
+										<input type="password" name="password_confirmation" placeholder="Password"
+										       required
+										       aria-label
+										       class="form-control"/>
+										<span class="input-group-text border-right-0 rounded-0 show-password"
+										      title="Show Password" style="cursor: pointer">
+									<i class="bi bi-eye"></i></span>
+									</div>
+								</div>
+							</div>
+							<div class="d-grid">
+								<button type="submit"
+								        class="fw-bold text-uppercase btn btn-primary ld-ext-right">
+									SIGN UP <i class="fas fa-user-plus"></i><span class="ld ld-ring ld-spin"></span>
+								</button>
+							</div>
+						</form>
+						<footer class="d-flex justify-content-center small auth_footer">
+							<a href="javascript:void(0)">Terms & Conditions</a>
+						</footer>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -161,7 +171,7 @@
                     complete: (xhr) => {
                         let err = eval("(" + xhr.responseText + ")");
 
-                        if(err.status !== true) submitButton.prop('disabled', false).removeClass('running')
+                        if (err.status !== true) submitButton.prop('disabled', false).removeClass('running')
                     }
                 })
             }
