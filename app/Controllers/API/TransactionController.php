3b7d0302c5cd1d3e15efd71e130ac09516e30d7f
Nabcellent <nabcellent.dev@gmail.com>
2021-11-12 00:49:44 +0300

    API - Adding transaction endpoints...😴 Afasali muninyonge

--- app/Controllers/API/TransactionController.php
+++ app/Controllers/API/TransactionController.php
@@ -6,6 +6,7 @@ use App\Models\MpesaStkCallback;
 use App\Models\PayPalCallback;
 use App\Models\Transaction;
 use App\Models\Wallet;
+use Carbon\Carbon;
 use CodeIgniter\RESTful\ResourceController;
 use Config\Services;
 use Exception;
@@ -23,6 +24,8 @@ class TransactionController extends ResourceController
      * @return mixed
      */
     public function index(): mixed {
+        $options = $this->request->getVar();
+
         try {
             $transactions = [
                 'wallet' => Transaction::with([
@@ -35,7 +38,8 @@ class TransactionController extends ResourceController
                     'payable_type',
                     'type',
                     'id',
-                    'status'
+                    'status',
+                    'created_at',
                 ]),
                 'paypal' => Transaction::with([
                     'payable' => function(MorphTo $morphTo) {
@@ -46,7 +50,8 @@ class TransactionController extends ResourceController
                     'payable_type',
                     'type',
                     'id',
-                    'status'
+                    'status',
+                    'created_at',
                 ]),
                 'mpesa'  => Transaction::with([
                     'payable' => function(MorphTo $morphTo) {
@@ -63,11 +68,41 @@ class TransactionController extends ResourceController
                     'payable_type',
                     'type',
                     'id',
-                    'status'
+                    'status',
+                    'created_at',
                 ]),
             ];
 
-            $transactions['mpesa']->prepend(count($transactions['mpesa']), 'count');
+            $transactions = collect($transactions)->map(function($transaction) use ($options) {
+                if(isset($options['from_date'])) {
+                    $fromDate = Carbon::createFromFormat('d-m-Y', $options['from_date'])->format('Y-m-d h:i:s');
+                    $toDate = isset($options['to_date'])
+                        ? Carbon::createFromFormat('d-m-Y', $options['to_date'])->format('Y-m-d h:i:s')
+                        : now();
+                    $transaction = $transaction->whereBetween('created_at', [$fromDate, $toDate]);
+                }
+
+                $transaction->prepend(count($transaction), 'count');
+
+                return $transaction;
+            });
+
+            if(isset($options['pay_method'])) {
+                $rules = [
+                    'pay_method' => 'in_list[mpesa,paypal,wallet]'
+                ];
+                $messages = [
+                    'pay_method' => ['email' => 'The email provided is already in use.']
+                ];
+
+                if(!$this->validate($rules, $messages)) {
+                    return $this->failValidationErrors($this->validator->getErrors());
+                }
+
+                return $this->respond($transactions[$options['pay_method']]);
+            }
+
+            $transactions->prepend($transactions->sum('count'), 'total_transactions');
 
             return $this->respond($transactions);
         } catch (Exception $e) {
